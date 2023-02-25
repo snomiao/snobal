dd59df7275e112e0126af5ae8bab1878957a8671
snomiao <snomiao@gmail.com>
2023-02-25 10:09:01 +0000

    Feb 25, 2023, 10:09 AM

--- pages/index.tsx
+++ pages/index.tsx
@@ -64,7 +64,7 @@ export default function Home() {
                 .replace(/ /g, "")
                 .replace(/\n\n/g, "\n")
                 .replace(
-                  /(.*?)(\+?[¥\\yYvV])([\d,]+)(?:・_)?\n(.*?)\n(\d\d\d\d)\/(\d\d)\/(\d\d)/g,
+                  /(.*?)(\+?[¥\\yYvV])([\d,]+)(?:・_)?\n(?:(.*?)\n)?(\d\d\d\d)\/(\d\d)\/(\d\d)/g,
                   (_, pos, signal, money, note, yyyy, MM, dd) => {
                     const sign = signal.startsWith("+") ? 1 : -1;
                     const cost = -sign * Number(money.replace(/,/, ""));
