3e62db57cefa4be4c51bbd9f8c1a3915c7377bc6
snomiao <snomiao@gmail.com>
2023-02-25 10:16:39 +0000

    Feb 25, 2023, 10:16 AM

--- pages/index.tsx
+++ pages/index.tsx
@@ -68,14 +68,17 @@ export default function Home() {
                   (_, pos, signal, money, noteLine, yyyy, MM, dd) => {
                     const sign = signal.startsWith("+") ? 1 : -1;
                     const cost = -sign * Number(money.replace(/,/, ""));
-                    const notes = (noteLine as string)
-                      .trim()
-                      .replace(/( )[E風回](.*?駅)/, (_, s, cho) => s + cho);
+                    const notes = [
+                      pos,
+                      (noteLine as string)
+                        .trim()
+                        .replace(/( )[E風回](.*?駅)/, (_, s, cho) => s + cho),
+                    ].filter(Boolean).join(" ");
                     const payee = notes.match("交通機関")
                       ? "交通機関"
                       : "UNKNOWN";
                     const s = [
-                      `${yyyy}-${MM}-${dd} * "${payee}" "${pos} ${notes}"`,
+                      `${yyyy}-${MM}-${dd} * "${payee}" "${notes}"`,
                       cost < 0 &&
                         `   Equity:Receivable:Assets:SuicaXR ${cost.toFixed(
                           2
