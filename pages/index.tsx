6b49f5ea7a7f7404cf5b2717156102df02470662
snomiao <snomiao@gmail.com>
2023-02-26 06:11:12 +0000

    Feb 26, 2023, 6:11 AM

--- pages/index.tsx
+++ pages/index.tsx
@@ -38,7 +38,7 @@ export default function Home() {
           };
           img.src = URL.createObjectURL(file);
           {
-            const lang = "eng+jpn+chn_sim";
+            const lang = "eng+jpn+chi_sim";
             setLoading((loading) => loading + 1);
             toast("Image pasted! processing");
             const job = await Tesseract.recognize(img, lang)
